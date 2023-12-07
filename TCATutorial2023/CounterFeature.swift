a7e0e95801d1f78af65c872e7e89be0c9b9d3be5
Daniel Bergquist <daniel.bergquist@hey.com>
2023-12-07 16:04:05 -0700

    Add fact scaffolding

--- TCATutorial2023/CounterFeature.swift
+++ TCATutorial2023/CounterFeature.swift
@@ -12,10 +12,13 @@ import SwiftUI
 struct CounterFeature {
     struct State: Equatable {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
 
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
 
@@ -24,9 +27,17 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
+
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .none
+
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
             }
         }
@@ -61,6 +72,22 @@ struct CounterView: View {
                     .padding()
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
+                }
+                Button("Fact") {
+//                    viewStore.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                if viewStore.isLoading {
+                    ProgressView()
+                } else if let fact = viewStore.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
                 }
             }
         }
