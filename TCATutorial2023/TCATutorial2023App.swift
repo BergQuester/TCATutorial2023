fd5919ad6cde1f922aa73bc204527287c056eba2
Daniel Bergquist <daniel.bergquist@hey.com>
2023-12-07 15:32:37 -0700

    Initial Commit

---
+++ TCATutorial2023/TCATutorial2023App.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorial2023App.swift
+//  TCATutorial2023
+//
+//  Created by Daniel Bergquist on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorial2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
