f6402c7cda89af8ab6cb63f3c88fae34bd6b3fea
Daniel Bergquist <daniel.bergquist@hey.com>
2023-12-14 16:18:46 -0700

    Add Navigation project

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Daniel Bergquist on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
