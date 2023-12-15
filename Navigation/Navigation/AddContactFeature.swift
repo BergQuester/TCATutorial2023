945e89a66af8eef0c59bfde06ee463dc2c9a28a5
Daniel Bergquist <daniel.bergquist@hey.com>
2023-12-15 16:08:15 -0700

    Use delegate actions and the DismissEffect in place of the parent feature snooping on all child actions

--- Navigation/Navigation/AddContactFeature.swift
+++ Navigation/Navigation/AddContactFeature.swift
@@ -16,18 +16,31 @@ struct AddContactFeature {
 
     enum Action {
         case cancelButtonTapped
+        case delegate(Delegate)
         case saveButtonTapped
         case setName(String)
+
+        enum Delegate: Equatable {
+            case saveContact(Contact)
+        }
     }
+
+    @Dependency(\.dismiss) var dismiss
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .cancelButtonTapped:
+                return .run { _ in await dismiss() }
+
+            case .delegate:
                 return .none
 
             case .saveButtonTapped:
-                return .none
+                return .run { [contact = state.contact] send in
+                    await send(.delegate(.saveContact(contact)))
+                    await dismiss()
+                }
 
             case let .setName(name):
                 state.contact.name = name
