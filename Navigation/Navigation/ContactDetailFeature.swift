be9b4e0defeb81f6f898f6d29a19ab9091b316c4
Daniel Bergquist <daniel.bergquist@hey.com>
2023-12-21 16:20:28 -0700

    Delete a contact from detail view

--- Navigation/Navigation/ContactDetailFeature.swift
+++ Navigation/Navigation/ContactDetailFeature.swift
@@ -11,17 +11,51 @@ import SwiftUI
 @Reducer
 struct ContactDetailFeature {
     struct State: Equatable {
+        @PresentationState var alert: AlertState<Action.Alert>?
         let contact: Contact
     }
 
     enum Action: Equatable {
-
+        case alert(PresentationAction<Alert>)
+        case delegate(Delegate)
+        case deleteButtonTapped
+        enum Alert {
+            case confirmDeletion
+        }
+        enum Delegate {
+            case confirmDeletion
+        }
     }
-
+    
+    @Dependency(\.dismiss) var dismiss
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+            case .alert(.presented(.confirmDeletion)):
+                return .run { send in
+                    await send(.delegate(.confirmDeletion))
+                    await self.dismiss()
+                }
+            case .alert:
+                return .none
+            case .delegate:
+                return .none
+            case .deleteButtonTapped:
+                state.alert = .confirmDeletion
+                return .none
+            }
+        }
+        .ifLet(\.$alert, action: \.alert)
+    }
+}
 
+extension AlertState where Action == ContactDetailFeature.Action.Alert {
+    static var confirmDeletion: Self {
+        Self {
+            TextState("Are you sure?")
+        } actions: {
+            ButtonState(role: .destructive, action: .confirmDeletion) {
+                TextState("Delete")
             }
         }
     }
@@ -33,10 +67,13 @@ struct ContactDetailView: View {
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             Form {
-
+                Button("Delete") {
+                    viewStore.send(.deleteButtonTapped)
+                }
             }
             .navigationBarTitle(Text(viewStore.contact.name))
         }
+        .alert(store: store.scope(state: \.$alert, action: \.alert))
     }
 }
 
